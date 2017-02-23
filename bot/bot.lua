bb65c6f985103015a3cf3ce3d8ac35d280429cad
Tihooteam <vakhi33vakhi@gmail.com>
2017-02-23 08:53:11 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -71,7 +71,7 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {157059515},
+    sudo_users = {205258325},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
